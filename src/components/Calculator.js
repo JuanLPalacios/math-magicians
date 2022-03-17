5032f1b03e96052a215a506f0e0fe019e7869d36
JuanLP <juan.luis.palacios.p@gmail.com>
2022-03-17 14:12:15 -0500

    Add calculator components

---
+++ src/components/Calculator.js
@@ -0,0 +1,35 @@
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+      <div className="result">0</div>
+
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="orange">÷</button>
+
+      <button type="button">7</button>
+      <button type="button">8/-</button>
+      <button type="button">9</button>
+      <button type="button" className="orange">x</button>
+
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="orange">-</button>
+
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="orange">+</button>
+
+      <button type="button" className="w-2">0</button>
+      <button type="button">.</button>
+      <button type="button" className="orange">=</button>
+    </div>
+  );
+}
+
+export default Calculator;
